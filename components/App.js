c8aa355235ed61cce0b8bf4f8b6bde915a291742
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-14 09:24:30 -0500

    Updating page with different JS lib names

--- components/App.js
+++ components/App.js
@@ -4,26 +4,53 @@ class App extends React.Component {
   constructor(){
     super();
         
+    this.selectRandomLibraries = this.selectRandomLibraries.bind(this);
+    this.randomFromArray = this.randomFromArray.bind(this);
+    
     this.state = {
       score: 0,
       realLibraries: [],
-      fakeLibraries: []
+      fakeLibraries: [],
+      selectedLibraries: []
     }
   }
   
   componentWillMount(){
     fetch('https://js-or-bs-server.herokuapp.com/fake_libraries')
       .then(res => res.json())
-      .then(fake_libs => this.setState(fake_libs));
+      .then(fake_libs => this.setState(
+        {fakeLibraries: fake_libs.fake_libraries}
+      ));
     fetch('https://js-or-bs-server.herokuapp.com/libraries')
       .then(res => res.json())
-      .then(libs => this.setState({realLibraries: libs.library}));
+      .then(libs => this.setState(
+        {realLibraries: libs.library},
+        this.selectRandomLibraries
+      ));
+  }
+  
+  randomFromArray(libArray){
+    return libArray[Math.floor(Math.random() * libArray.length)]
+  }
+  
+  selectRandomLibraries() {
+    let randFake = this.randomFromArray(this.state.fakeLibraries);
+    let randReal = this.randomFromArray(this.state.realLibraries);
+    var shuffledLibraries = Math.random() > .5 ? [randFake, randReal] : [randReal, randFake]
+    this.setState({selectedLibraries: shuffledLibraries})
   }
   
   render() {
     return (
       <div className="app">
-        I am an app
+        Current score: {this.state.score}
+        <ul>
+          {this.state.selectedLibraries.map(lib => {
+            return(
+              <li key={lib.id}>{lib.name}</li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
