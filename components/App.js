2ed62be70a22a72ac4c528b062768afcb12ca52f
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-13 22:49:36 -0500

    Remove bundle.js and update app to store all date

--- components/App.js
+++ components/App.js
@@ -15,21 +15,15 @@ class App extends React.Component {
     fetch('https://js-or-bs-server.herokuapp.com/fake_libraries')
       .then(res => res.json())
       .then(fake_libs => this.setState(fake_libs));
-    // fetch('https://js-or-bs-server.herokuapp.com/libraries')
-    //   .then(res2 => res2.json())
-    //   .then(libraries => {
-    //     this.setState(libraries)
-    //   })
+    fetch('https://js-or-bs-server.herokuapp.com/libraries')
+      .then(res => res.json())
+      .then(libs => this.setState({realLibraries: libs.library}));
   }
   
   render() {
     return (
       <div className="app">
         I am an app
-        {console.log(this.state)}
-        <ul>
-          // {this.state.fake_libraries.map(library => <li>{library.name}</li>)}
-        </ul>
       </div>
     );
   }
