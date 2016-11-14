dd896171d76ed8789de6ecc05884f1d0ef753350
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-13 21:05:43 -0500

    updated babelify version to 6.4.0 so it can read jsx

--- components/App.js
+++ components/App.js
@@ -1,11 +1,13 @@
 const React = require('react');
 
 class App extends React.Component {
-  render(){
-    return(
-      <div>
+  render() {
+    return (
+      <div className="app">
         I am an app
       </div>
-    )
+    );
   }
 }
+
+module.exports = App;
---
+++ index.js
@@ -0,0 +1,7 @@
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./components/App');
+
+const $main = document.getElementById('main');
+ReactDOM.render(<App />, $main);
