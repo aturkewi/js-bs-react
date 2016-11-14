9c3952091112ca7e963163bba89942ae9d5bfc9b
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-13 22:45:14 -0500

    add fetch call for fake libraries

--- bundle.js
+++ bundle.js
@@ -1,13 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var React = require('react');
 
@@ -17,16 +17,51 @@ var App = (function (_React$Component) {
   function App() {
     _classCallCheck(this, App);
 
-    _get(Object.getPrototypeOf(App.prototype), "constructor", this).apply(this, arguments);
+    _get(Object.getPrototypeOf(App.prototype), 'constructor', this).call(this);
+
+    this.state = {
+      score: 0,
+      realLibraries: [],
+      fakeLibraries: []
+    };
   }
 
   _createClass(App, [{
-    key: "render",
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      var _this = this;
+
+      fetch('https://js-or-bs-server.herokuapp.com/fake_libraries').then(function (res) {
+        return res.json();
+      }).then(function (fake_libs) {
+        return _this.setState(fake_libs);
+      });
+      // fetch('https://js-or-bs-server.herokuapp.com/libraries')
+      //   .then(res2 => res2.json())
+      //   .then(libraries => {
+      //     this.setState(libraries)
+      //   })
+    }
+  }, {
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "div",
-        { className: "app" },
-        "I am an app"
+        'div',
+        { className: 'app' },
+        'I am an app',
+        console.log(this.state),
+        React.createElement(
+          'ul',
+          null,
+          '// ',
+          this.state.fake_libraries.map(function (library) {
+            return React.createElement(
+              'li',
+              null,
+              library.name
+            );
+          })
+        )
       );
     }
   }]);
--- components/App.js
+++ components/App.js
@@ -1,10 +1,35 @@
 const React = require('react');
 
 class App extends React.Component {
+  constructor(){
+    super();
+        
+    this.state = {
+      score: 0,
+      realLibraries: [],
+      fakeLibraries: []
+    }
+  }
+  
+  componentWillMount(){
+    fetch('https://js-or-bs-server.herokuapp.com/fake_libraries')
+      .then(res => res.json())
+      .then(fake_libs => this.setState(fake_libs));
+    // fetch('https://js-or-bs-server.herokuapp.com/libraries')
+    //   .then(res2 => res2.json())
+    //   .then(libraries => {
+    //     this.setState(libraries)
+    //   })
+  }
+  
   render() {
     return (
       <div className="app">
         I am an app
+        {console.log(this.state)}
+        <ul>
+          // {this.state.fake_libraries.map(library => <li>{library.name}</li>)}
+        </ul>
       </div>
     );
   }
